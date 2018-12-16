dfd72c036d133207784393e4a72c020e3b4f68a4
charlie <cjpdee@gmail.com>
2018-12-16 15:14:51 +0000

    started building basic object & structure for app

---
+++ src/js/objects/Oscillator.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import store from "../store";
+
+class Oscillator extends React.Component {
+    constructor() {
+        this.id = store.getOscillatorCount();
+        this.wavetable = wavetable
+
+        store.incrementOscCount();
+    }
+
+    delete() {
+        // delete the object & dom element
+        // then:
+        store.decrementOscCount()
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Oscillator {this.id}</h2>
+                <select>
+                    <option value="sine/sqr"></option>
+                    <option value="sine/saw"></option>
+                </select>
+                <input type="range" id="wt pos" />
+                <input type="range" id="volume" />
+                <input type="range" id="lowpass" />
+                <input type="range" id="resonance" />
+            </div>
+        );
+    }
+
+    getPitch(note) {
+        var notes = [
+            A4,B4,C4...
+        ];
+        switch(note) {
+            case 'A4':
+                // do thingy
+        }
+    }
+}
+
+export default Oscillator;
---
+++ src/js/store.js
@@ -0,0 +1,14 @@
+var store = {
+    values : {
+        bpm : 90,
+        noteLength : '1/4',
+        pitches : {
+            // figure out an algorithm to divide the pitches,
+            // don't hard code this
+            // also should be a function and not an obj
+        }
+    },
+    getPitch(note) {
+        // 
+    }
+}
